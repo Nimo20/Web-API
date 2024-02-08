0bcd376ed839f2bcecbeb7e2572706d1f9c8323b
Nimo <shirille21@gmail.com>
2024-02-08 10:40:42 -0600

    .

---
+++ Assets/script.js
@@ -0,0 +1,43 @@
+var quiz = [
+    {
+        question: " What does CSS stand for?",
+        options: [" a) Cascading Script Style",
+            "b) Cascading Style Sheets",
+            "c) Computer Style Sheets",
+            "d) Creative Styling Solutions"],
+        Answer: " b) Cascading Style Sheets"
+    },
+    {
+        question: " How is CSS typically used in web development?",
+        options: ["a) To define the structure and content of a web page",
+            "b) To add interactivity and functionality to a web page",
+            "c) To describe the presentation and style of a web page",
+            "d) To manage server - side operations of a web page"],
+        Answer: " c) To describe the presentation and style of a web page."
+    },
+    {
+        question: "Which HTML attribute is used to apply CSS styles directly to an HTML element ?",
+        options: [" a) style",
+            "b) css",
+            "c) class",
+            "d) id"],
+        Answer: "a) style"
+    },
+    {
+        question: "Which CSS property is used to control the size and layout of boxes in CSS ?",
+        options: ["a) color",
+            "b) font - family",
+            "c) width",
+            "d) background - color"],
+        Answer: " c) width"
+    },
+    {
+        question: "What is the correct CSS syntax to select an element with a specific class?",
+        options: ["a) .class",
+            "b) #class",
+            " c) .class-name",
+            "d) class:"],
+        Answer: "a) .class}"
+    }
+
+];
